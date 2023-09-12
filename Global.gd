9f36e47037bca03ab125fc2077a86befc9f9df98
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-09-12 13:03:00 +0700

    setup: generate board

---
+++ Global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+export var CELL_SIZE = 32
+var HALF_CELLSIZE = CELL_SIZE/2
+export var board_rows = 20 
+export var board_cols = 10
+
+var board_width = board_cols * CELL_SIZE
+var board_height = board_rows * CELL_SIZE
