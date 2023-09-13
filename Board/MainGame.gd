7334411a04772f792ea161d39fa97c978db7822e
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-09-13 23:00:41 +0700

    feat: randomize spawn

--- Board/MainGame.gd
+++ Board/MainGame.gd
@@ -5,8 +5,29 @@ var board_pos = null
 # var a = 2
 # var b = "text"
 var current_block = null 
+var rng =RandomNumberGenerator.new()
+func pick_random_block(): 
+	var i = rng.randi()%7
+	var scene = ""
+	match i: 
+		0: 
+			scene="res://TetrisBlocks/CBlock.tscn"
+		1: 
+			scene="res://TetrisBlocks/IBlock.tscn"
+		2: 
+			scene="res://TetrisBlocks/JBlock.tscn"
+		3: 
+			scene="res://TetrisBlocks/LBlock.tscn"
+		4: 
+			scene="res://TetrisBlocks/SBlock.tscn"
+		5: 
+			scene="res://TetrisBlocks/TBlock.tscn"
+		6: 
+			scene="res://TetrisBlocks/ZBlock.tscn"
+	var block = load(scene).instance()
+	return block
 func spawn_block(): 
-	var block = preload("res://TetrisBlocks/CBlock.tscn").instance()
+	var block = pick_random_block()
 	add_child(block)
 	block.global_position=$spawn_pos.global_position
 	current_block = block 
@@ -24,6 +45,8 @@ func setup_board():
 func _ready():
 	setup_board()
 	spawn_block()
+	rng.randomize()
+
 	pass 
 
 
